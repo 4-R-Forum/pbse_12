4cdeca9d324cd3b1447cb79d690d456d76dcb43d
4-R-Forum <8607712+4-R-Forum@users.noreply.github.com>
2023-06-17 13:19:47 -0400

    CS Pin15, SO Pin 14

--- main.ts
+++ main.ts
@@ -1,24 +1,24 @@
-/**
- * There can be several square edged event blocks
- */
+let y = 0
+// There can be several square edged event blocks
 let x = 0
-let temp = 1;
+let highByte = 0
+let lowByte = 0
+let temp = 1
 loops.everyInterval(2000, function () {
-    let y = 0
     basic.showString("" + (temp))
 })
 basic.forever(function () {
-    pins.setPull(DigitalPin.P15, PinPullMode.PullNone);
-    pins.digitalWritePin(DigitalPin.P14, 0); // set CS low
-    basic.pause(1);
-    let highByte = pins.spiWrite(0);
-    let lowByte = pins.spiWrite(0);
-    pins.digitalWritePin(DigitalPin.P15, 1);
-
+    pins.setPull(DigitalPin.P14, PinPullMode.PullNone)
+    // set CS low
+    pins.digitalWritePin(DigitalPin.P15, 0)
+    basic.pause(1)
+    highByte = pins.spiWrite(0)
+    lowByte = pins.spiWrite(0)
+    pins.digitalWritePin(DigitalPin.P14, 1)
     if (lowByte & (1 << 2)) {
-        temp = -1000;
+        temp = -1000
     } else {
         let value = (highByte << 5 | lowByte >> 3);
-        temp = value >> 2;
+temp = value >> 2;
     }
 })
