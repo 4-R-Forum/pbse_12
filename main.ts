23141a327e2f39c23995635fda3dea02d01e5c1f
4-R-Forum <8607712+4-R-Forum@users.noreply.github.com>
2023-06-17 10:17:23 -0400

    Refactor extension, set CS P14 low

--- main.ts
+++ main.ts
@@ -1,11 +1,24 @@
 /**
  * There can be several square edged event blocks
  */
-let y = 0
 let x = 0
+let temp = 1;
 loops.everyInterval(2000, function () {
-    basic.showString("" + (y))
+    let y = 0
+    basic.showString("" + (temp))
 })
 basic.forever(function () {
-    y = input.max6675Temperature(DigitalPin.P15)
+    pins.setPull(DigitalPin.P15, PinPullMode.PullNone);
+    pins.digitalWritePin(DigitalPin.P14, 0); // set CS low
+    basic.pause(1);
+    let highByte = pins.spiWrite(0);
+    let lowByte = pins.spiWrite(0);
+    pins.digitalWritePin(DigitalPin.P15, 1);
+
+    if (lowByte & (1 << 2)) {
+        temp = -1000;
+    } else {
+        let value = (highByte << 5 | lowByte >> 3);
+        temp = value >> 2;
+    }
 })
