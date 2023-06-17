a7c916026b6c9dfa42e878c9dc3092e1ef1954fc
4-R-Forum <8607712+4-R-Forum@users.noreply.github.com>
2023-06-17 13:30:11 -0400

    parameterize pin variables

--- main.ts
+++ main.ts
@@ -1,6 +1,6 @@
-let y = 0
 // There can be several square edged event blocks
-let x = 0
+let cs = DigitalPin.P15
+let so = DigitalPin.P14
 let highByte = 0
 let lowByte = 0
 let temp = 1
@@ -8,13 +8,13 @@ loops.everyInterval(2000, function () {
     basic.showString("" + (temp))
 })
 basic.forever(function () {
-    pins.setPull(DigitalPin.P14, PinPullMode.PullNone)
+    pins.setPull(so, PinPullMode.PullNone)
     // set CS low
-    pins.digitalWritePin(DigitalPin.P15, 0)
+    pins.digitalWritePin(cs, 0)
     basic.pause(1)
     highByte = pins.spiWrite(0)
     lowByte = pins.spiWrite(0)
-    pins.digitalWritePin(DigitalPin.P14, 1)
+    pins.digitalWritePin(so, 1)
     if (lowByte & (1 << 2)) {
         temp = -1000
     } else {
